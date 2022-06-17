5410fb9ce71cb30338cf0bb059e54be87a9a3308
Ankush263 <ankushbanik263@gmail.com>
2022-06-17 15:57:30 +0530

    create base

---
+++ Day-1.sol
@@ -0,0 +1,12 @@
+/*
+Q. Your first program in Solidity
+   Objective - In this challenge, we review some basic concepts that will get you started with this series. You will need to use the same (or similar) syntax to read input and write output in challenges throughout Codedamn.
+
+   Task - To complete this challenge, you must print Hello Solidity. Use str as a variable to store value.
+*/
+
+pragma solidity >=0.5.0 < 0.9.0;
+
+contract HelloSolidity{
+   
+}
