367639deb0c646d9eaff6ca9d538c4de8c137155
Ankush263 <ankushbanik263@gmail.com>
2022-06-18 14:28:33 +0530

    Day-2 solution

--- Day-2.sol
+++ Day-2.sol
@@ -11,5 +11,14 @@ In the same way you have to create a local variable and initialised it with 20 a
 pragma solidity >=0.5.0 <0.9.0;
 
 contract Function{
-    
+    uint x = 10;
+
+    function returnStateVariable() public view returns(uint){
+      return x;
+    }
+
+    function returnLocalVariable() public pure returns(uint){
+      uint y = 20;
+      return y;
+    }
 }
