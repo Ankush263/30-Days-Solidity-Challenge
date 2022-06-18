a4805fa387b6e1833eb3f2b9a1b76e82f8cae31c
Ankush263 <ankushbanik263@gmail.com>
2022-06-18 14:16:40 +0530

    Day-2 question

---
+++ Day-2.sol
@@ -0,0 +1,15 @@
+/*
+Create functions
+Task-
+
+To complete this challenge, you have to create a state variable and initialised it with 10 and then you have to create a function returnStateVariable( ) which returns the value of the created state variable .
+
+In the same way you have to create a local variable and initialised it with 20 and then you have to create a function returnLocalVariable( ) which returns the value of the created local variable.
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Function{
+    
+}
