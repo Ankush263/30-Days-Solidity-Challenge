84c41fcdd827e70291f8333599f6ed204566478e
Ankush263 <ankushbanik263@gmail.com>
2022-06-19 13:16:51 +0530

    Day-3 problem

---
+++ Day-3.sol
@@ -0,0 +1,19 @@
+/*
+Pass an argument to function
+Task
+
+To complete this challenge, do the following.
+
+1)Create a state variable of uint type (need not to be public).
+
+2)Create a set ( ) and with one argument. And set the value of the created state variable with the value passed as an argument to the set ( ).
+
+3)Then create another function get ( ) which returns the value of the state variable.
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 < 0.9.0;
+
+contract FunctionArgument{
+
+}
