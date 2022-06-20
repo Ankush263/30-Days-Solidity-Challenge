b624d9e55b8bb1da3a669f4ce512b85a45ef0fff
Ankush263 <ankushbanik263@gmail.com>
2022-06-20 15:03:22 +0530

    Day-4 problem

---
+++ Day-4.sol
@@ -0,0 +1,16 @@
+/*
+Solidity Basics
+Task
+
+Create a function evaluate(int a , int b).
+Subtract the difference of a and b from the sum of a and b.
+Return the result of the above task from the evaluate()
+Note - Function will be public.
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract day4 {
+    
+}
