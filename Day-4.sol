39da7108534b1c15c930ced88abd7fe1d23ba73f
Ankush263 <ankushbanik263@gmail.com>
2022-06-20 15:10:56 +0530

    Day-4 solution

--- Day-4.sol
+++ Day-4.sol
@@ -2,9 +2,9 @@
 Solidity Basics
 Task
 
-Create a function evaluate(int a , int b).
-Subtract the difference of a and b from the sum of a and b.
-Return the result of the above task from the evaluate()
+1. Create a function evaluate(int a , int b).
+2. Subtract the difference of a and b from the sum of a and b.
+3. Return the result of the above task from the evaluate()
 Note - Function will be public.
 */
 
@@ -12,5 +12,7 @@ Note - Function will be public.
 pragma solidity >=0.5.0 <0.9.0;
 
 contract day4 {
-    
+    function evaluate(int a, int b)public pure returns(int){
+      return (a+b)-(a-b);
+    }
 }
