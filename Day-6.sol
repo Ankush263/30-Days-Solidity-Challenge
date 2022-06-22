e109f9448ca80e61e7869d7857dbbd2b15a917d9
Ankush263 <ankushbanik263@gmail.com>
2022-06-22 14:56:52 +0530

    Day-6 solution

--- Day-6.sol
+++ Day-6.sol
@@ -11,5 +11,7 @@ Note - Function will be public.
 pragma solidity >=0.5.0 <0.9.0;
 
 contract day6 {
-     
+  function average(int a, int b, int c)public pure returns(int){
+    return (a + b + c)/3;
+  }
 }
