8af2f3ed2a968a49969d126262651659f6ed0513
Ankush263 <ankushbanik263@gmail.com>
2022-06-22 14:54:40 +0530

    Day-6 question

---
+++ Day-6.sol
@@ -0,0 +1,15 @@
+/*
+Find average
+Task
+
+1. Create a function average(int a,int b,int c). Where a>0,b>0,c>0.
+2. Return the average of a,b and c.
+Note - Function will be public.
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract day6 {
+     
+}
