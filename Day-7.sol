45ee1bf8554fe53376dd552b6774e9657dea20fd
Ankush263 <ankushbanik263@gmail.com>
2022-06-23 14:45:04 +0530

    Day-7 question

---
+++ Day-7.sol
@@ -0,0 +1,18 @@
+/*
+Find the sum of digits
+Task
+
+1. Create a function digitSum(int n). Where n>0.
+2. Return the sum of digit for n.
+For Example - If n=345 then digitSum() must return 12 i.e. 3+4+5 = 12
+If n=909 then digitSum() must return 18 i.e. 9+0+9 = 18
+
+Note - Function will be public.
+*/
+
+// SPDX-License-Identifier: aIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract day1 {
+    
+}
