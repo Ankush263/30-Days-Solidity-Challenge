e2c5e056b6c48e8ef06874b88ba7c20b839c1ddd
Ankush263 <ankushbanik263@gmail.com>
2022-06-23 14:51:45 +0530

    Day-7 solution

--- Day-7.sol
+++ Day-7.sol
@@ -13,6 +13,15 @@ Note - Function will be public.
 // SPDX-License-Identifier: aIT
 pragma solidity >=0.5.0 <0.9.0;
 
-contract day1 {
-    
+contract day7 {
+    function digitSum(int n)public pure returns(int){
+      int x = 0;
+      int sum = 0;
+      while(n != 0){
+        x = n%10;
+        sum += x;
+        n = n/10;
+      }
+      return sum;
+    }
 }
