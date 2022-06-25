2253d1482dff93c0976370a41c3f0a0edf53d1e7
Ankush263 <ankushbanik263@gmail.com>
2022-06-25 14:03:42 +0530

    Day-9 question

---
+++ Day-9.sol
@@ -0,0 +1,19 @@
+/*
+Prime Number
+Task
+
+A prime number is a whole number greater than 1. It has exactly two factors, that is, 1 and the number itself.
+Create a function prime(uint n) . This prime() will check whether n is a prime number or not.
+If n is a prime number then prime() returns 1 and n is not prime then prime() must return 0.
+For Example - If n=103 then prime() must return 1 as 103 is a prime number. If n=200 then prime() must return 0 as 200 is not a prime number.
+
+Note - Function will be public.
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Day3 {
+  
+      
+}
