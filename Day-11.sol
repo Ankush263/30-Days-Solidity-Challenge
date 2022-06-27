5efb41bde5d861ae68b2e67556d7f47bbc520236
Ankush263 <ankushbanik263@gmail.com>
2022-06-27 19:49:30 +0530

    Day-11 question

---
+++ Day-11.sol
@@ -0,0 +1,18 @@
+/*
+Palindrome
+Task
+
+1. A palindrome number is a number that is same after reverse. For example 545, 151, 34543 etc.
+2. Create a function palindrome(uint n) . Thispalindrome() will check whether n is a palindrome or not.
+3. If n is a palindrome then palindrome() returns 1 and n is not palindrome then palindrome() must return 0.
+For Example - If n=545 then palindrome() must return 1. If n=123 then palindrome() must return 0.
+
+Note - Function will be public.
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Day5 {
+   
+}
