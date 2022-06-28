e5a10f785aaabaaefe025bb6d13a0955334450fe
Ankush263 <ankushbanik263@gmail.com>
2022-06-28 11:46:20 +0530

    Day-12 question

---
+++ Day-12.sol
@@ -0,0 +1,16 @@
+/*
+Reverse a Number
+Task
+
+1. Create a function reverseDigit(uint n) . This reverseDigit() will do the reversal of the number n.
+For Example - If n=123 then reverseDigit() must return 321. If n=5342 then reverseDigit() must return 2435.
+
+Note - Function will be public.
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Day6 {
+    
+}
