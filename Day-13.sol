aa753354c1ea6af218935bfbd27ad48d30fc3b04
Ankush263 <ankushbanik263@gmail.com>
2022-06-29 21:20:11 +0530

    Day-13 question

---
+++ Day-13.sol
@@ -0,0 +1,15 @@
+/*
+Array Reversal
+Task
+
+1. Create a function reverseArray(array, length of array) . This reverseArray() will take two arguments - a dynamic uint type array and length of the array.
+2. The reverseArray() will reverse the array. For Example - If array =[2,5,9,11,1] then reverseArray() will return [1,11,9,5,2] If array =[90,20,30,10] then reverseArray() will return [10,30,20,90]
+Note - Function will be public.
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Day1 {
+    function reverseArray(uint[] arr, length of array)
+}
