98653e679f0e36f9dad0b511d6ef696259c68b6e
Ankush263 <ankushbanik263@gmail.com>
2022-06-29 21:38:52 +0530

    Day-11 answer

--- Day-13.sol
+++ Day-13.sol
@@ -11,5 +11,13 @@ Note - Function will be public.
 pragma solidity >=0.5.0 <0.9.0;
 
 contract Day1 {
-    function reverseArray(uint[] arr, length of array)
+  function reverseArray(uint[] memory arr, uint len) public pure returns(uint[] memory){
+    uint temp;
+    for(uint i = 0; i < len/2; i++){
+      temp = arr[i];
+      arr[i] = arr[len - i - 1];
+      arr[len - i - 1] = temp;
+    }
+    return arr;
+  }
 }
