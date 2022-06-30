ab12da28bddd578c021c2080561fa169c4944566
Ankush263 <ankushbanik263@gmail.com>
2022-06-30 20:51:50 +0530

    Day-14 solution

--- Day-14.sol
+++ Day-14.sol
@@ -11,5 +11,11 @@ Note - Function will be public.
 pragma solidity >=0.5.0 <0.9.0;
 
 contract Day2 {
+  function even(uint[] memory arr, uint length) public pure returns(uint[] memory){
     
+    for(uint i = 0; i < length; i++){
+      arr[i] = arr[i] * 2;
+    }
+    return arr;
+  }
 }
