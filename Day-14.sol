e1d33c823e6c184eebe4fbad6def3d112e181589
Ankush263 <ankushbanik263@gmail.com>
2022-06-30 20:37:23 +0530

    Day14 question

---
+++ Day-14.sol
@@ -0,0 +1,15 @@
+/*
+Array to even
+Task
+
+1. Create a function even(array, length of array) . This even() will take two arguments - a dynamic uint type array and length of the array.
+2. The even() will multiply each element of array with 2. For Example - If array =[2,5,11,1] then even() will return [4,10,22,2,] If array =[1,3,5,7] then even() will return [2,6,10,14]
+Note - Function will be public.
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Day2 {
+    
+}
