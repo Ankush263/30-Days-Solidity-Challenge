bac7b974069f10ea1eeab479a327b51bd07b7592
Ankush263 <ankushbanik263@gmail.com>
2022-07-01 20:41:20 +0530

    Day-15 question

---
+++ Day-15.sol
@@ -0,0 +1,15 @@
+/*
+Array Sorting
+Task
+
+1. Create a function sort(array, length of array) . This sort() will take two arguments - a dynamic uint type array and length of the array.
+2. The sort() will sort the array elements in ascending order. For Example - If array =[2,5,9,11,1] then sort() will return [1,2,5,9,11] If array =[90,20,30,10] then sort() will return [10,20,30,90]
+Note - Function will be public.
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Day3 {
+   
+}
