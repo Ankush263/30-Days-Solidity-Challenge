96c1e076e367490ace56fc90b2c3165f62bda2f7
Ankush263 <ankushbanik263@gmail.com>
2022-07-06 12:42:27 +0530

    update

--- contracts/Day-5.sol
+++ contracts/Day-5.sol
@@ -10,7 +10,7 @@ Note - Function will be public.
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
 
-contract day5 {
+contract Day5 {
     function find(int a)public pure returns(int){
       return a%3;
     }
