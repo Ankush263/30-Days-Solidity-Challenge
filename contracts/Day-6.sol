61813ad26cc1981d3a3add8798d8d2992d0185ee
Ankush263 <ankushbanik263@gmail.com>
2022-07-07 12:25:13 +0530

    updatee

--- contracts/Day-6.sol
+++ contracts/Day-6.sol
@@ -10,7 +10,7 @@ Note - Function will be public.
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
 
-contract day6 {
+contract Day6 {
   function average(int a, int b, int c)public pure returns(int){
     return (a + b + c)/3;
   }
